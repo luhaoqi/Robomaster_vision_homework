34e98d12832590a3665db94b696aaff53a325ead
luhaoqi <1171821456@qq.com>
2022-01-18 02:38:49 -0800

    homework2 first commit

--- homework2/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ homework2/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -54,5 +54,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/opencv_test.dir/DependInfo.cmake"
+  "CMakeFiles/homework2.dir/DependInfo.cmake"
   )
